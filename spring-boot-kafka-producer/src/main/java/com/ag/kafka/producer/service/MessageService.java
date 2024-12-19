5ccbb1b82b7462df4c9307424db31cf2fc916169
Adnan Ghazanfar <adnan.ghazanfar@ing.com>
2024-12-19 18:45:11 +0100

    kafka producer via conf

--- spring-boot-kafka-producer/src/main/java/com/ag/kafka/producer/service/MessageService.java
+++ spring-boot-kafka-producer/src/main/java/com/ag/kafka/producer/service/MessageService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageService {
 
-    @Value("${kafka.template.topic}")
+    @Value("${kafka.topic}")
     String topicName;
 
     final KafkaTemplate<String, String> kafkaTemplate;
